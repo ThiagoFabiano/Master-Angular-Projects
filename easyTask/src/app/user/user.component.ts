4ebe7a4b9e36a38042a954e4dc149c93f25d631d
Thiago <thiagoffabiano@gmail.com>
2025-07-24 11:51:51 -0300

    feat: exibe usuarios no app component

--- easyTask/src/app/user/user.component.ts
+++ easyTask/src/app/user/user.component.ts
@@ -1,27 +1,19 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-user';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]) 
-  imagePath = computed(() =>'assets/users/' + this.selectedUser().avatar)
 
-  // get imagePath(){
-  //   return 'assets/users/' + this.selectedUser.avatar
-  // }
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
-
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUser() {}
 }
